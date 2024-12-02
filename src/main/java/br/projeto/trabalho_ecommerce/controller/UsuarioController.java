895f5deaa554a4d01358c13f8812986ef7f50f86
guilhermefreitas450 <guilhermeoss450@gmail.com>
2024-12-01 23:27:33 -0300

    post usando dto2f

--- src/main/java/br/projeto/trabalho_ecommerce/controller/UsuarioController.java
+++ src/main/java/br/projeto/trabalho_ecommerce/controller/UsuarioController.java
@@ -1,12 +1,10 @@
 package br.projeto.trabalho_ecommerce.controller;
 
+import br.projeto.trabalho_ecommerce.dto.usuarioDTO;
 import br.projeto.trabalho_ecommerce.model.usuario;
 import br.projeto.trabalho_ecommerce.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,11 +15,26 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
 
+    @GetMapping
+    public List<usuario> findAll(){
+        return this.repository.findAll();
+    }
+
     @GetMapping("/{id}")
     public usuario findById(@PathVariable Integer id) {
         return this.repository.findById(id)
                 .orElseThrow(() ->
                         new IllegalArgumentException("Usuario nao pode ser encontrado"));
+    }
 
+@PostMapping 
+    public usuario save(usuarioDTO dto){
+        usuario usuario = new usuario();
+        usuario.setNome(dto.nome());
+        usuario.setEmail(dto.email());
+        usuario.setTelefone(dto.telefone());
+        usuario.setEndereco(dto.endereco());
+
+        return this.repository.save(usuario);
     }
 }
