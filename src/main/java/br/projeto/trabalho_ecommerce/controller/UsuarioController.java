fa45a893b0b63b2f625353003b454aaa18f92be9
guilhermefreitas450 <guilherme.da@grupointegrado.br>
2024-12-01 21:51:19 -0300

    findByiD

--- src/main/java/br/projeto/trabalho_ecommerce/controller/UsuarioController.java
+++ src/main/java/br/projeto/trabalho_ecommerce/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import br.projeto.trabalho_ecommerce.model.usuario;
 import br.projeto.trabalho_ecommerce.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,13 @@ public class UsuarioController {
     @GetMapping
     public List<usuario> findall(){
         return this.repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public usuario findById(@PathVariable Integer id){
+        return this.repository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Usuario nao pode ser encontrado"));
     }
 
 }
--- src/main/java/br/projeto/trabalho_ecommerce/model/usuario.java
+++ src/main/java/br/projeto/trabalho_ecommerce/model/usuario.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 
-
 @Entity
 @Table
 public class usuario {
